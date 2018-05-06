09e6a80f3110d4ad4e1de60e5aeebbedd38b5792
AlexV <asmodehn@gmail.com>
2018-05-06 09:31:37 +0200

    refactored a bit to be able to enforce using backported classes in python3. fixed an error where cache was not cleaned on activate on python3.

--- filefinder2/__init__.py
+++ filefinder2/__init__.py
@@ -11,17 +11,78 @@ import six
 
 import types
 import warnings
+import contextlib
 
 from ._utils import _ImportError
 
-from ._filefinder2 import activate, deactivate
-from ._filefinder2 import get_filefinder_index_in_path_hooks, get_pathfinder_index_in_meta_hooks
+from .machinery import PathFinder, FileFinder
+from .machinery import get_filefinder_index_in_path_hooks, get_pathfinder_index_in_meta_hooks
 
 # extra API (not exposed in importlib) useful when defining extensions of basic python import
-from ._fileloader2 import get_supported_file_loaders
+from .machinery import get_supported_file_loaders
+
 
 # Public API #########################################################
 
+
+# Making the activation explicit for now
+def activate(force=False):
+    """Install the path-based import components."""
+
+    global PathFinder, FileFinder, ff_path_hook
+
+    if (2, 7) <= sys.version_info < (3, 4):
+        if ff_path_hook not in sys.path_hooks:
+            sys.path_hooks.append(ff_path_hook)
+
+        if PathFinder not in sys.meta_path:
+            # Setting up the meta_path to change package finding logic
+            sys.meta_path.append(PathFinder)
+
+    elif sys.version_info >= (3, 4):  # valid from which py3 version ?
+        pass
+
+    else:
+        raise ImportError("filefinder2 : Unsupported python version")
+
+    # Resetting sys.path_importer_cache values,
+    # to support the (usual) case where we have an implicit package or a module inside an already loaded package,
+    # since we need to replace the default importer.
+    sys.path_importer_cache.clear()
+    # Note : without this, newly added filefinder.find_spec will NOT be called,
+    # Since filefinder was probably already cached for most locations.
+
+
+def deactivate(force=False):
+    if force or (2, 7) <= sys.version_info < (3, 4):
+        # CAREFUL : Even though we remove the path from sys.path,
+        # initialized finders will remain in sys.path_importer_cache
+
+        # removing metahook
+        sys.meta_path.pop(get_pathfinder_index_in_meta_hooks())
+        sys.path_hooks.pop(get_filefinder_index_in_path_hooks())
+
+        # Resetting sys.path_importer_cache to get rid of previous importers
+        sys.path_importer_cache.clear()
+
+    elif sys.version_info >= (3, 4):  # valid from which py3 version ?
+        pass
+
+    else:
+        raise ImportError("filefinder2 : Unsupported python version")
+
+
+@contextlib.contextmanager
+def enable_pep420():
+    """
+    Enabling support of pep420, now available on python2
+    :param force: if set, filefinder2 code will be enabled for python3 as well.
+    CAREFUL : this is only intended for import-time debugging purposes from python code.
+    :return:
+    """
+    activate()
+    yield
+    deactivate()
 
 try:
     from importlib import invalidate_caches
